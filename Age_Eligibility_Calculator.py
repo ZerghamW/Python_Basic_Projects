1a037bb5b47a9c82368aad532173cf31411052fe
ZerghamW <87094714+ZerghamW@users.noreply.github.com>
2022-05-18 06:24:29 +0500

    Create Age_Eligibility_Calculator.py

---
+++ Age_Eligibility_Calculator.py
@@ -0,0 +1,13 @@
+# Age Eligibility Calculator
+# Getting the age of the user in a variable
+
+age_user = int(input("Enter Your Age...")
+
+if age_user<18:
+    print("You are not eligible")
+elif age_user>85:
+    print("You are not eligible")
+else:
+    print("You are Eligible.... Congratulations!")
+               
+print("--- END ---")
